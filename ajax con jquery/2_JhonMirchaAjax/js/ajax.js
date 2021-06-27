1c48768f8bcd52a7cf4ad8c52ea18e71e7947e7d
Facundo Sebastian Cordoba <cordobafs@gmail.com>
2021-06-27 20:11:04 -0300

    asincronismo
    se agrega el asincronismo con  fetch api y  jquery

--- ajax con jquery/2_JhonMirchaAjax/js/ajax.js
+++ ajax con jquery/2_JhonMirchaAjax/js/ajax.js
@@ -1,36 +1,33 @@
 (()=>{
-    const xhr = new XMLHttpRequest() ,
+     const xhr = new XMLHttpRequest() ,
      $xhr = document.getElementById('xhr'),
      $fragmento = document.createDocumentFragment(); // todas las variables  con signo dolar hacen referencia a elementos del dom
 
      xhr.addEventListener("readystatechange", e=>{
-
         if(xhr.readyState === 4){
              if(xhr.status>=200 && xhr.status<300){
                  console.log('exito');
-                 console.log(xhr.responseText);
                 let json =JSON.parse(xhr.responseText);
-                console.log(json);
-               json.forEach(element => {
-                   const $li = document.createElement("li");
-                   $li.innerHTML= `${element.name}---${element.email}---${element.phone}`;
-                   $fragmento.appendChild($li);
+                //console.log(json);
+                json.forEach(element => {
+                const $li = document.createElement("li");
+                 $li.innerHTML= `${element.name}---${element.email}---${element.phone}`;
+                $fragmento.appendChild($li);
                }); // forEach
-
                $xhr.appendChild($fragmento);
-
              }
              else{
                  
                 let message = (xhr.statusText==="")?'a ocurrido un error' : xhr.statusText;
                 //console.log(`error:`);
                 //console.log(xhr);
-                $xhr.innerHTML =`Error: ${xhr.status} ${xhr.message} por favor contactese con cgcet `;
+                $xhr.innerHTML =`Error: ${xhr.status} ${message} por favor contactese con cgcet `;
 
              }
-        }
+        } // end if
 
      }); // cuando exista algun cambio de estado  llamar a la callback
+     console.log('esto se ejecuta igual');
 
 
      //xhr.open("GET","https://jsonplaceholder.typicode.com/users");
@@ -38,41 +35,56 @@
      xhr.send();
 })();
 
-
-
-
-function subirTransferencia(identificador) {
-    const xhr = new XMLHttpRequest(); // creo el elemento xhr
-
-    xhr.addEventListener("readystatechange", e => {
-
-        if (xhr.readyState === 4) {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                console.log('exito');
-                console.log(xhr.responseText);
-                let json = JSON.parse(xhr.responseText);
-                console.log(json);
+/** autoejecutable api fetch **/
+(()=>{
+      const $fetch = document.getElementById('fetchApi'),
+            $fragmento = document.createDocumentFragment(); // todas las variables  con signo dolar hacen referencia a elementos del dom
+      fetch("https://jsonplaceholder.typicode.com/users"
+      ).then(
+        (respuestaServidor)=>{
+                /* respuestaServidor.blob()  devuelve formato de blob*/
+                /* respuestaServidor.text()   devuelve formato texto*/
+            return respuestaServidor.ok ? respuestaServidor.json() : Promise.reject(respuestaServidor); //json
+         }    
+    ).then(
+        json => {            
                 json.forEach(element => {
-                    const $li = document.createElement("li");
-                    $li.innerHTML = `${element.name}---${element.email}---${element.phone}`;
-                }); // forEach
-
-
-            }
-            else {
-
-                let message = (xhr.statusText === "") ? 'a ocurrido un error' : xhr.statusText;
-                //console.log(`error:`);
-                //console.log(xhr);
-
-            }
+                const $li = document.createElement("li");
+                $li.innerHTML= `${element.name}---${element.email}---${element.phone}`;
+                $fragmento.appendChild($li);
+                });
+                $fetch.appendChild($fragmento);
         }
+    ).catch(
+        (errorServidor) =>{
+            let message = errorServidor.statusText||'a ocurrido un error';
+            $fetch.innerHTML = `Error ${errorServidor.status} ${message}`;
+        }
+    ).finally(
+       ()  => console.log("esto se ejecutara independientemente del resultado de la promesa fetch")       
+    );
+})();
 
-    }); // cuando exista algun cambio de estado  llamar a la callback
 
-    let url = @Url.Action("Transferencia", "Certificaciones", new { id = "param-id" }); // seteo la url
-    url = url.replace("param-id", identificador); // modifico la url con el valor de la transferencia.
+(()=>{
 
-    xhr.open("GET", "@Url.Action("Transferencia", "Certificaciones", new {id = 1235 })");
-    xhr.send();
-} // funcion para subir  los comprobantes  y las transferencias.
+    const $jquery = document.getElementById('jquery'),
+    $fragmento = document.createDocumentFragment();
+ // Assign handlers immediately after making the request,
+// and remember the jqXHR object for this request
+ $.ajax( "https://jsonplaceholder.typicode.com/users" )
+       .done(function(data) {
+                data.forEach(element => {
+                    const $li = document.createElement("li");
+                    $li.innerHTML= `${element.name}---${element.email}---${element.phone}`;
+                    $fragmento.appendChild($li);
+                });
+                 $jquery.appendChild($fragmento);
+            })
+        .fail(function() {
+                         alert( "error" );
+            })
+         .always(function() {
+                         alert( "complete" );
+            });
+})();
